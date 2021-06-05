f26c3d8fc1b9ba3b64bdab691fad7234ef2c65d0
hx-w <herixth@163.com>
2021-06-05 19:49:56 +0800

    flask api

--- api/index.py
+++ api/index.py
@@ -1,11 +1,17 @@
 # -*- coding: utf-8 -*-
-from flask import Flask
+from flask import Flask, Response, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def hello_world(path):
+    key = request.args.get('key')
+    return Response("<h1>Flask</h1><p>You visited: /%s</p><p>key=%s</p>"
+                        % (path, key), mimetype="text/html")
     return 'Hello World'
+
 
 if __name__ == '__main__':
     app.run()
