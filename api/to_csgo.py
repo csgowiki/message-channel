1f2c536fb077ba2e1e71cdb07d5c7440de248afe
hx-w <herixth@163.com>
2021-06-07 23:16:11 +0800

    tcp test

--- api/to_csgo.py
+++ api/to_csgo.py
@@ -1,10 +1,10 @@
 # -*- coding: utf -*-
+import os
 import socket
+import ujson
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
-
-socket_cotainer = {}
 
 
 def create_socket():
@@ -18,29 +18,61 @@ def to_csgo_view():
 
 @app.route('/api/tcp_connect', methods=['POST'])
 def connect_tcp():
-    global socket_container
-    sv_remark = request.form.get("sv_remark")
-    qq_group = request.form.get("qq_group")
-    token = request.form.get("token")
-
-    if (qq_group, sv_remark) in socket_container.keys():
-        return jsonify({"status": "warning", "message": "tcp connect exist"})
+    socket_container = os.getenv('SOCKET_CONTAINER')
+    if not socket_container:
+        socket_container = {}
     else:
-        socket_container[(qq_group, sv_remark)] = create_socket()
-        return jsonify({"status": "ok", "message": "created a tcp connect"})
-    return jsonify({"status": "error"})
+        socket_container = ujson.loads(socket_container)
+    sv_remark = request.form.get("sv_remark")
+    sv_host = request.form.get("sv_host")
+    qq_group = request.form.get("qq_group")
+
+    success = False
+    if qq_group in socket_container.keys():
+        for value in socket_container[qq_group]:
+            if value == [sv_remark, sv_host]:
+                return jsonify({
+                    "status": "warning",
+                    "message": "tcp connect exist"
+                })
+        socket_container[qq_group].append((sv_remark, sv_host))
+        success = True
+    else:
+        socket_container[qq_group] = [(sv_remark, sv_host)]
+        success = True
+
+    if success:
+        os.environ['SOCKET_CONTAINER'] = ujson.dumps(socket_container)
+        return jsonify({
+            "status": "ok",
+            "message": "[success] tcp connect created"
+        })
+    else:
+        return jsonify({"status": "error"})
 
 
 @app.route('/api/tcp_close', methods=['POST'])
 def close_tcp():
-    global socket_container
-    sv_remark = request.form.get("sv_remark")
-    qq_group = request.form.get("qq_group")
-    token = request.form.get("token")
-
-    if (qq_group, sv_remark) in socket_container.keys():
-        del socket_container[(qq_group, sv_remark)]
-        return jsonify({"status": "ok", "message": "closed a tcp connect"})
+    socket_container = os.getenv('SOCKET_CONTAINER')
+    if not socket_container:
+        socket_container = {}
     else:
-        return jsonify({"status": "warning", "message": "no exist tcp connect"})
-    return jsonify({"status": "error"})
+        socket_container = ujson.loads(socket_container)
+    sv_remark = request.form.get("sv_remark")
+    sv_host = request.form.get("sv_host")
+    qq_group = request.form.get("qq_group")
+
+    if qq_group in socket_container.keys():
+        for value in socket_container[qq_group]:
+            if value == [sv_remark, sv_host]:
+                socket_container[qq_group].remove([sv_remark, sv_host])
+                os.environ['SOCKET_CONTAINER'] = ujson.dumps(socket_container)
+                return jsonify({
+                    "status": "ok",
+                    "message": "[success] tcp connect closed"
+                })
+
+    return jsonify({
+        "status": "warning",
+        "message": "tcp connect not exist"
+    })
