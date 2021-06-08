c2537f72a31d94f0441c9b18a78ac3b2a612449f
hx-w <herixth@163.com>
2021-06-08 11:45:57 +0800

    socket api fix

--- api/to_csgo.py
+++ api/to_csgo.py
@@ -1,48 +1,59 @@
 # -*- coding: utf -*-
 import os
+import time
+import copy
 import socket
 import ujson
-from flask import Flask, jsonify, request
+from flask import Flask, json, jsonify, request
 
 app = Flask(__name__)
 
 
-def create_socket():
-    return socket.socket()
+def load_container() -> dict:
+    container = os.getenv('SOCKET_CONTAINER')
+    if not container:
+        container = {}
+    else:
+        container = ujson.loads(container)
+    return container
 
+def dump_container(container: dict) -> bool:
+    try:
+        os.environ['SOCKET_CONTAINER'] = ujson.dumps(container)
+        return True
+    except:
+        return False
 
 @app.route('/api/to_csgo', methods=['POST'])
 def to_csgo_view():
     return jsonify({"status": "error"})
 
 
-@app.route('/api/tcp_connect', methods=['POST'])
+@app.route('/api/tcp_create', methods=['POST'])
 def connect_tcp():
-    socket_container = os.getenv('SOCKET_CONTAINER')
-    if not socket_container:
-        socket_container = {}
-    else:
-        socket_container = ujson.loads(socket_container)
+    socket_container = load_container()
     sv_remark = request.form.get("sv_remark")
     sv_host = request.form.get("sv_host")
     qq_group = request.form.get("qq_group")
 
     success = False
     if qq_group in socket_container.keys():
-        for value in socket_container[qq_group]:
-            if value == [sv_remark, sv_host]:
+        for value_idx in range(len(socket_container[qq_group])):
+            if socket_container[qq_group][value_idx][:2] == [sv_remark, sv_host]:
+                socket_container[qq_group][value_idx] = [sv_remark, sv_host, time.time()]
+                dump_container(socket_container)
                 return jsonify({
                     "status": "warning",
                     "message": "tcp connect exist"
                 })
-        socket_container[qq_group].append((sv_remark, sv_host))
+        socket_container[qq_group].append([sv_remark, sv_host, time.time()])
         success = True
     else:
-        socket_container[qq_group] = [(sv_remark, sv_host)]
+        socket_container[qq_group] = [[sv_remark, sv_host, time.time()]]
         success = True
 
     if success:
-        os.environ['SOCKET_CONTAINER'] = ujson.dumps(socket_container)
+        dump_container(socket_container)
         return jsonify({
             "status": "ok",
             "message": "[success] tcp connect created"
@@ -53,20 +64,16 @@ def connect_tcp():
 
 @app.route('/api/tcp_close', methods=['POST'])
 def close_tcp():
-    socket_container = os.getenv('SOCKET_CONTAINER')
-    if not socket_container:
-        socket_container = {}
-    else:
-        socket_container = ujson.loads(socket_container)
+    socket_container = load_container()
     sv_remark = request.form.get("sv_remark")
     sv_host = request.form.get("sv_host")
     qq_group = request.form.get("qq_group")
 
     if qq_group in socket_container.keys():
-        for value in socket_container[qq_group]:
-            if value == [sv_remark, sv_host]:
-                socket_container[qq_group].remove([sv_remark, sv_host])
-                os.environ['SOCKET_CONTAINER'] = ujson.dumps(socket_container)
+        for value_idx in range(len(socket_container[qq_group])):
+            if socket_container[qq_group][value_idx][:2] == [sv_remark, sv_host]:
+                del socket_container[qq_group][value_idx]
+                dump_container(socket_container)
                 return jsonify({
                     "status": "ok",
                     "message": "[success] tcp connect closed"
@@ -76,3 +83,55 @@ def close_tcp():
         "status": "warning",
         "message": "tcp connect not exist"
     })
+
+@app.route('/api/socket_info', methods=['GET'])
+def socket_info():
+    web_token = request.args.get('web_token')
+    access_token = os.getenv('ACCESS_TOKEN')
+    if web_token != access_token:
+        return jsonify({
+            "status": "error",
+            "message": "token invalid"
+        })
+    socket_container = load_container()
+    return jsonify({
+        "status": "ok",
+        "result": socket_container
+    })
+
+@app.route('/api/socket_refresh', methods=['GET'])
+def socket_refresh():
+    web_token = request.args.get('web_token')
+    access_token = os.getenv('ACCESS_TOKEN')
+    if web_token != access_token:
+        return jsonify({
+            "status": "error",
+            "message": "token invalid"
+        })
+
+    socket_container = load_container()
+    if len(socket_container.keys()) == 0:
+        return jsonify({
+            "status": "ok",
+            "message": "empty session"
+        })
+
+    old_socket_container = copy.deepcopy(socket_container)
+    for mykey in list(socket_container.keys()):
+        if len(socket_container[mykey]) == 0:
+            continue
+        for myvalue_idx in range(len(socket_container[mykey])):
+            if time.time() - float(socket_container[mykey][myvalue_idx][2]) >= 24 * 60 * 60:
+                del socket_container[mykey][myvalue_idx]
+        if len(socket_container[mykey]) == 0:
+            socket_container.pop(mykey)
+
+    dump_container(socket_container)
+    return jsonify({
+        "status": "ok",
+        "old_session": old_socket_container,
+        "new_session": socket_container
+    })
+
+
+app.run()
